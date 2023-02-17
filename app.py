01581339ddb9bc1f552c92e6dab799e718d6708e
LD <chiyc88@gmail.com>
2023-02-17 22:58:35 +0800

    Update - automatically open the web and type in username, password

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 """This python file will do the AutoClass job."""
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 import utilities as utils
 
 config = utils.read_config()
 
-print(config.get('username'))
-print(config.get('password'))
+driver = webdriver.Chrome()
+
+
+def driver_send_keys(locator, key):
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located(locator)).send_keys(key)
+
+
+def driver_click(locator):
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located(locator)).click()
+
+
+def login():
+    driver.get('https://course.fcu.edu.tw/')
+    driver_send_keys((By.ID, "ctl00_Login1_UserName"), config.get("username"))
+    driver_send_keys((By.ID, "ctl00_Login1_Password"), config.get("password"))
+
+
+login()
+time.sleep(100)
