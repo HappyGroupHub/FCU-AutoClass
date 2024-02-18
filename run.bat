915ba1e03c718f975401ae96122022fc54828b48
LD <chiyc88@gmail.com>
2024-02-18 17:30:12 +0800

    Fix bug - make run.bat works properly on both win10 and win11

--- run.bat
+++ run.bat
@@ -3,7 +3,9 @@
 @echo off
 title FCU-AutoClass
 chcp 65001
-set currentDate=%date:~-4,4%-%date:~4,2%-%date:~7,2%-%time:~0,2%%time:~3,2%%time:~6,2%
+
+for /F "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
+set currentDate=%datetime:~0,8%-%datetime:~8,6%
 FCU-AutoClass.exe 2> "logs\logs-%currentDate%.txt"
 
 for /f "delims=" %%a in ("logs\logs-%currentDate%.txt") do (
@@ -11,6 +13,7 @@ for /f "delims=" %%a in ("logs\logs-%currentDate%.txt") do (
         goto :End
     )
 )
+
 echo 現在可按視窗右上角的 X 關閉程式 否則將於5sec後自動重新啟動
 timeout /t 5 > nul
 goto Start
